9181cfd38b53a20171eb06c8cddbcf8bd1af5731
sagarkumarpatel <sagarpatel98212@gmail.com>
2026-03-13 19:16:10 +0530

    implementing SignUp and SignIn using Material UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing";
+import Authentication from "./pages/authentication";
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
     <Route path="/" element={<LandingPage/>}/>
+    <Route path="/auth" element={<Authentication/>}/>
       </Routes>
     </Router>
   );
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -23,7 +23,7 @@ export default function LandingPage(){
                     <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved Ones</h1>
                     <p>Cover a distance by Apana Video Call</p>
                     <div role="button">
-                        <Link to={"/home"}>Get Started</Link>
+                        <Link to={"/auth"}>Get Started</Link>
                     </div>
                 </div>
                 <div>
