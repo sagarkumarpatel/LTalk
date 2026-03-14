8c7a2bba62bb50467eb6b252d975d3c76cff29c7
sagarkumarpatel <sagarpatel98212@gmail.com>
2026-03-14 19:27:45 +0530

    implementing authentication on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
-import LandingPage from "./pages/landing";
-import Authentication from "./pages/authentication";
-function App() {
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
+import History from './pages/history';
 
+function App() {
   return (
-  
-    <Router>
-      <Routes>
-    <Route path="/" element={<LandingPage/>}/>
-    <Route path="/auth" element={<Authentication/>}/>
-      </Routes>
-    </Router>
+    <div className="App">
+
+      <Router>
+
+        <AuthProvider>
+
+
+          <Routes>
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+
+      </Router>
+    </div>
   );
 }
 
-export default App
+export default App;
