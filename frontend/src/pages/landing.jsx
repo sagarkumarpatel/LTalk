1cf98ffe524876164d790574e1cc0957536a6c8f
sagarkumarpatel <sagarpatel98212@gmail.com>
2026-03-16 18:01:00 +0530

    implementing history feature

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,35 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../App.css";
+import React from 'react'
+import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
+export default function LandingPage() {
 
 
-export default function LandingPage(){
-    return(
-        <div className="landingPageContainer">
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
             <nav>
-                <div className="navHeader">
-                    <h2>Let's Talk</h2>
+                <div className='navHeader'>
+                    <h2>Apna Video Call</h2>
                 </div>
-                <div className="navList">
-                    <p>Join as Guest</p>
-                    <p>Register</p>
-                    <div role="button">
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
                         <p>Login</p>
                     </div>
                 </div>
             </nav>
+
+
             <div className="landingMainContainer">
                 <div>
-                    <h1><span style={{color:"#FF9839"}}>Connect</span> with your loved Ones</h1>
-                    <p>Cover a distance by Apana Video Call</p>
-                    <div role="button">
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Apna Video Call</p>
+                    <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
                 </div>
                 <div>
-                    <img src="/callingImage3.webp" alt="" />
+
+                    <img src="/mobile.png" alt="" />
+
                 </div>
             </div>
+
+
+
         </div>
     )
 }
